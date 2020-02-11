a55e45204a362ce5ae0353e3705db5db91905048
Fabian Vilers <fabian.vilers@dev-one.com>
2020-02-11 16:15:21 +0100

    Remove search result ads

--- src/content.js
+++ src/content.js
@@ -9,11 +9,23 @@ const removeHomepageAds = () => {
   ads.forEach(ad => ad.replaceWith(""));
 };
 
+const removeSearchResultAds = () => {
+  const listItems = document.querySelectorAll(
+    ".mp-Listing.mp-Listing--list-item"
+  );
+  const ads = Array.from(listItems).filter(item =>
+    item.querySelector(".mp-Listing-seller-link")
+  );
+
+  ads.forEach(ad => ad.replaceWith(""));
+};
+
 // Watch for changes being made to the DOM tree
 const targetNode = document.querySelector("body");
 const config = { attributes: true, childList: true, subtree: true };
 const callback = (mutationsList, observer) => {
   removeHomepageAds();
+  removeSearchResultAds();
 };
 const observer = new MutationObserver(callback);
 observer.observe(targetNode, config);
