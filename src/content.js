16a76d6bdbbe95fcd8a5de48e72bfdf85d3ea57f
Fabian Vilers <fabian.vilers@dev-one.com>
2020-02-11 16:04:06 +0100

    Remove home page ads

--- src/content.js
+++ src/content.js
@@ -1,6 +1,19 @@
+const removeHomepageAds = () => {
+  const feedItems = document.querySelectorAll(".mp-Listing-card.feed-item");
+  const ads = Array.from(feedItems).filter(item => {
+    const link = item.querySelector("a.mp-Listing-card-clickable-container");
+
+    return link && link.href ? !link.href.endsWith("&previousPage=home") : true;
+  });
+
+  ads.forEach(ad => ad.replaceWith(""));
+};
+
 // Watch for changes being made to the DOM tree
 const targetNode = document.querySelector("body");
 const config = { attributes: true, childList: true, subtree: true };
-const callback = (mutationsList, observer) => {};
+const callback = (mutationsList, observer) => {
+  removeHomepageAds();
+};
 const observer = new MutationObserver(callback);
 observer.observe(targetNode, config);
