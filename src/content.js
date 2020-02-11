3adecdee41b042993b622cc84eb23199c1593afc
Fabian Vilers <fabian.vilers@dev-one.com>
2020-02-11 15:55:07 +0100

    Add the mutation observer

--- src/content.js
+++ src/content.js
@@ -1,1 +1,6 @@
-console.info("Extension loaded.");
+// Watch for changes being made to the DOM tree
+const targetNode = document.querySelector("body");
+const config = { attributes: true, childList: true, subtree: true };
+const callback = (mutationsList, observer) => {};
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
